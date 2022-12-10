18f174d0acab9af4330de165f5138d4b46682d8a
Masaya Shida <masayashida@gmail.com>
2022-12-10 19:45:25 +0700

    working starting and remaining balance

--- employee_system/models.py
+++ employee_system/models.py
@@ -8,12 +8,20 @@ class MyUser(AbstractUser):
 class LeaveBalance(models.Model):
     user = models.ForeignKey(MyUser, verbose_name=("user"), on_delete=models.CASCADE)
 
-    leave_annual = models.PositiveIntegerField(verbose_name='Annual Leave', default=18)
-    leave_sick = models.PositiveIntegerField(verbose_name='Sick Leave', default=100)
-    leave_work_home = models.PositiveIntegerField(verbose_name='Work From Home', default=20)
-    leave_maternity = models.PositiveIntegerField(verbose_name='Maternity Leave', default=0)
+    starting_leave_annual = models.PositiveIntegerField(verbose_name='Starting Balance - Annual Leave', default=18)
+    used_leave_annual = models.PositiveIntegerField(verbose_name='Used Balance - Annual Leave', default=0)
+    
+    starting_leave_sick = models.PositiveIntegerField(verbose_name='Starting Balance - Sick Leave', default=100)
+    used_leave_sick = models.PositiveIntegerField(verbose_name='Used Balance - Sick Leave', default=0)
+
+    starting_leave_work_home = models.PositiveIntegerField(verbose_name='Starting Balance - Work From Home', default=20)
+    used_leave_work_home = models.PositiveIntegerField(verbose_name='Used Balance - Work From Home', default=0)
+
+    starting_leave_maternity = models.PositiveIntegerField(verbose_name='Starting Balance - Maternity Leave', default=0)
+    used_leave_maternity = models.PositiveIntegerField(verbose_name='Used Balance - Maternity Leave', default=0)
+
 
     def __str__(self):
         return f"""
-        {self.id}. [{self.user.username}]: Annual: {self.leave_annual} | Sick: {self.leave_sick} | Home: {self.leave_work_home} | Maternity: {self.leave_maternity}
+        {self.id}. {self.user.username}'s Remaining Balance: Annual: {self.starting_leave_annual - self.used_leave_annual} | Sick: {self.starting_leave_sick - self.used_leave_sick} | Home: {self.starting_leave_work_home - self.used_leave_work_home} | Maternity: {self.starting_leave_maternity - self.used_leave_maternity}
         """
