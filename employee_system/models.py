bdb45b20522e6c9786b8c9bdb0195801e382d241
Masaya Shida <masayashida@gmail.com>
2022-12-10 19:59:29 +0700

    add dept manager status

--- employee_system/models.py
+++ employee_system/models.py
@@ -4,20 +4,26 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class MyUser(AbstractUser):
     email = models.EmailField('email_address', max_length=254, blank=False)
+    dept_manager = models.BooleanField(default=False, verbose_name="Department Manager")
+
 
 class LeaveBalance(models.Model):
     user = models.ForeignKey(MyUser, verbose_name=("user"), on_delete=models.CASCADE)
 
     starting_leave_annual = models.PositiveIntegerField(verbose_name='Starting Balance - Annual Leave', default=18)
+    planned_leave_annual = models.PositiveIntegerField(verbose_name='Planned - Annual Leave', default=0)
     used_leave_annual = models.PositiveIntegerField(verbose_name='Used Balance - Annual Leave', default=0)
     
     starting_leave_sick = models.PositiveIntegerField(verbose_name='Starting Balance - Sick Leave', default=100)
+    planned_leave_sick = models.PositiveIntegerField(verbose_name='Planned - Sick Leave', default=0)
     used_leave_sick = models.PositiveIntegerField(verbose_name='Used Balance - Sick Leave', default=0)
 
     starting_leave_work_home = models.PositiveIntegerField(verbose_name='Starting Balance - Work From Home', default=20)
+    planned_leave_work_home = models.PositiveIntegerField(verbose_name='Planned - Work From Home', default=0)
     used_leave_work_home = models.PositiveIntegerField(verbose_name='Used Balance - Work From Home', default=0)
 
     starting_leave_maternity = models.PositiveIntegerField(verbose_name='Starting Balance - Maternity Leave', default=0)
+    planned_leave_maternity = models.PositiveIntegerField(verbose_name='Planned - Maternity Leave', default=0)
     used_leave_maternity = models.PositiveIntegerField(verbose_name='Used Balance - Maternity Leave', default=0)
 
 
