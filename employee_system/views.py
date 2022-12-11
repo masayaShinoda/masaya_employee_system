4650950f60df1446b0344860f72f9ec8efde1e2a
Masaya Shida <masayashida@gmail.com>
2022-12-11 21:17:23 +0700

    got cards working correctly

--- employee_system/views.py
+++ employee_system/views.py
@@ -2,14 +2,28 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.urls import reverse
+from .models import LeaveBalance
+
+from django.core import serializers
 
 # Create your views here.
 def index(request):
+    if request.user.is_authenticated:
+        leave_balance = serializers.serialize('json', [LeaveBalance.objects.all().get(user=request.user), ])
+    else:
+        leave_balance = None
+
+
     context = {
         'metadata': {
-            "author": "Masaya Shida",
+            'author': 'Masaya Shida',
             'page_title': 'Home',
-        }
+        },
+        'data': {
+            'leave_balance': leave_balance,
+        },
+        'misc': {
+        },
     }
 
     return render(request, "employee_system/index.html", context)
